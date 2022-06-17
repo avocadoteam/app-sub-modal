8e496182de9f6bb85a774c152704c68b580967b0
Konstantin <konstantin.miheev@sensearena.com>
2022-06-17 13:46:00 +0200

    change to react

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import preact from '@preact/preset-vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/app-sub-modal/',
-  plugins: [preact(), vanillaExtractPlugin()],
+  plugins: [react(), vanillaExtractPlugin()],
 });
